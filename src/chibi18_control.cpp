9371b11cacbffbcdf6b23b673483af717c239863
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-02-16 14:47:38 +0900

    testnagai.txtを削除

---
+++ src/chibi18_control.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <geometry_msgs/Pose.h>
+#include <tf/transform_broadcaster.h>
+#include "roomba_500driver_meiji/RoombaCtrl.h"
+#include <iostream>
+#include <math.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Float32.h>
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/imgproc/imgproc.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include <std_msgs/String.h>
+
+int main(){
+  while(ros::ok()){
+
+  }
+  return 0;
+}
