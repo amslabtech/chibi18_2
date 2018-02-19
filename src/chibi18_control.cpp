95c6b2ca02191aaee8d0fe45962b7eacee452c9e
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-02-19 10:04:55 +0900

    新しいノードを追加、launchを作成

--- src/chibi18_control.cpp
+++ src/chibi18_control.cpp
@@ -61,7 +61,7 @@ int main(int argc, char** argv){
     }
     roomba_velocity.cntl.linear.x = vx; 
 
-    float omega_z = 0.5 * atan2((_target.x - roomba_position.pose.pose.position.x), (_target.y - roomba_position.pose.pose.position.y));//0.5は適当
+    float omega_z = 0.5 * atan2((_target.y - roomba_position.pose.pose.position.y), (_target.x - roomba_position.pose.pose.position.x));//0.5は適当
     if(omega_z < -1.0){
       omega_z = -1.0;
     }else if(omega_z > 1.0){
---
+++ src/set_target.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <geometry_msgs/Pose2D.h>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "set_target");
+  ros::NodeHandle nh;
+
+  ros::Publisher target_pub = nh.advertise<geometry_msgs::Pose2D>("/chibi18/target", 100);
+
+  ros::Rate loop_rate(10);
+
+  geometry_msgs::Pose2D target;
+  target.x = 3.0;
+  target.y = 0.0;
+  target.theta = 0.0;
+
+  while(ros::ok()){
+    target_pub.publish(target);
+    
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
