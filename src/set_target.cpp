05ec303b16d89e0b1cf9b45b77330d967e48006e
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-02-21 12:37:34 +0900

    test.launchの中に引数を書けるようにした

--- src/set_target.cpp
+++ src/set_target.cpp
@@ -7,14 +7,18 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "set_target");
   ros::NodeHandle nh;
+  ros::NodeHandle local_nh("~");
+
+  float distance;
+  local_nh.getParam("DISTANCE", distance);
 
   ros::Publisher target_pub = nh.advertise<geometry_msgs::Pose2D>("/chibi18/target", 100);
 
   ros::Rate loop_rate(10);
 
   geometry_msgs::Pose2D target;
-  target.x = 0.5;
-  target.y = 0.5;
+  target.x = distance;
+  target.y = 0.0;
   target.theta = 0.0;
   while(ros::ok()){
     target_pub.publish(target);
