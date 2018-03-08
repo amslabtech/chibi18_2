42f8572ad57ff31a6ea5f2ba3f27dd9cef29a47a
kazuki takahahsi <tkhsh.schoen@gmail.com>
2018-03-08 23:52:21 +0900

    globalpathplanning用のソースファイルを作った。中身も少し書いた

---
+++ src/global_planner.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
+
+nav_msgs::OccupancyGrid map;
+nav_msgs::Path global_path;
+
+
+void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
+{
+  map = *msg;
+}
+
+int get_map_data(float, float);
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "global_planner");
+  ros::NodeHandle nh;
+  ros::NodeHandle local_nh;
+
+  ros::Subscriber map_sub = nh.subscribe("/map", 100, map_callback);
+
+  ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/chibi18/global_path", 100);
+
+  ros::Rate loop_rate(10);
+
+  global_path.header.frame_id = "map";
+
+  geometry_msgs::PoseStamped pose;
+  pose.pose.position.x = 0;
+  pose.pose.position.y = 0;
+  pose.pose.orientation.w = 1;
+  global_path.poses.push_back(pose);
+  pose.pose.position.x = 1;
+  global_path.poses.push_back(pose);
+
+  while(ros::ok()){
+    if(!map.data.empty()){
+      //std::cout << map.data.size() << std::endl;
+      float x = 1.00;
+      float y = -1.20;
+      std::cout << get_map_data(x, y) << std::endl;
+    }
+    if(!global_path.poses.empty()){
+      path_pub.publish(global_path);
+    }
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
+
+int get_map_data(float x, float y)
+{
+  int data = map.data[int((map.info.width*(y-map.info.origin.position.y)+(x-map.info.origin.position.x))/map.info.resolution)];
+  return data;
+}
