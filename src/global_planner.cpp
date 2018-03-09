cac5277832444dba652e0111aaee2fdfaff58e19
kazuki takahahsi <tkhsh.schoen@gmail.com>
2018-03-09 19:13:40 +0900

    ヒューリスティックの計算まで実装

--- src/global_planner.cpp
+++ src/global_planner.cpp
@@ -11,13 +11,14 @@ nav_msgs::Path global_path;
 
 nav_msgs::OccupancyGrid _cost_map;//for debug
 
-class Grid
+class Cell 
 {
 public:
-  Grid(void)
+  Cell(void)
   {
     is_wall = false;
     cost = 1;
+    heuristic = -1;
   }
 
   int cost;
@@ -26,7 +27,14 @@ public:
   bool is_wall;
 };
 
-std::vector<Grid> cells;
+std::vector<Cell> cells;
+std::vector<int> open_list;
+std::vector<int> close_list;
+
+
+int get_grid_data(float, float);
+int get_index(float, float);
+void calculate_heuristic(int);
 
 void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
 {
@@ -41,10 +49,27 @@ void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
   _cost_map.header = map.header;
   _cost_map.info = map.info;
   _cost_map.data.resize(map.info.height*map.info.width);
+  for(int i=0;i<map.info.height;i++){
+    for(int j=0;j<map.info.width;j++){
+      std::cout << cells[j*map.info.width+i].is_wall << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  //calculate AStar
+  int start_index = get_index(start.point.x, start.point.y);
+  int goal_index = get_index(goal.point.x, goal.point.y);
+  calculate_heuristic(goal_index);
+
+  for(int i=0;i<map.info.height;i++){
+    for(int j=0;j<map.info.width;j++){
+      std::cout << std::setw(2) << cells[j*map.info.width+i].heuristic << " ";
+    }
+    std::cout << std::endl;
+  }
+  open_list.push_back(start_index);
 }
 
-int get_grid_data(float, float);
-int get_index(float, float);
 
 int main(int argc, char** argv)
 {
@@ -55,7 +80,8 @@ int main(int argc, char** argv)
   local_nh.getParam("START_X", start.point.x);
   local_nh.getParam("START_Y", start.point.y);
   local_nh.getParam("GOAL_X", goal.point.x);
-  local_nh.getParam("GOALY", goal.point.y);
+  local_nh.getParam("GOAL_Y", goal.point.y);
+  //std::cout << goal.point.x << " " << goal.point.y<<std::endl;
  
   start.header.frame_id = "map";
   goal.header.frame_id = "map";
@@ -81,15 +107,8 @@ int main(int argc, char** argv)
 
   while(ros::ok()){
     if(!map.data.empty()){
-      std::cout << map.data.size() << std::endl;
-      float x = -1.48;
-      float y = -1.02;
-      //std::cout << get_grid_data(x, y) << std::endl;
-      
       //calculate AStar
-      int start_index = get_index(start.point.x, start.point.y);
-      int goal_index = get_index(goal.point.x, goal.point.y); 
-
+      
 
       for(int i=0;i<_cost_map.data.size();i++){
         _cost_map.data[i] = cells[i].cost;
@@ -124,5 +143,67 @@ int get_index(float x, float y)
     y = ((int)(10*(2*y)-1))/20.0;
   }
   int index = int((map.info.width*(y-map.info.origin.position.y)+(x-map.info.origin.position.x))/map.info.resolution);
+  std::cout << index << " " << x << " " << y <<std::endl;
   return index;
+}
+
+void calculate_heuristic(int goal_index)
+{
+  int h = 0;
+  cells[goal_index].heuristic = 0;
+  bool flag = false;
+  while(1){
+    flag = false;
+    for(int i=0;i<map.info.height;i++){
+      for(int j=0;j<map.info.width;j++){
+        if(cells[j*map.info.width+i].heuristic == h){
+          flag = true;
+          if(j-1>=0){
+            if(cells[(j-1)*map.info.width+i].heuristic<0){
+              cells[(j-1)*map.info.width+i].heuristic = h+1;//i, j-1
+            }
+          }
+          if(j+1<map.info.width){
+            if(cells[(j+1)*map.info.width+i].heuristic<0){
+              cells[(j+1)*map.info.width+i].heuristic = h+1;//i, j+1
+            }
+          }
+          if(i+1<map.info.height){
+            if(cells[j*map.info.width+(i+1)].heuristic<0){
+              cells[j*map.info.width+(i+1)].heuristic = h+1;//i+1, j
+            }
+            if(j-1>=0){
+              if(cells[(j-1)*map.info.width+(i+1)].heuristic<0){
+                cells[(j-1)*map.info.width+(i+1)].heuristic = h+1;//i+1, j-1
+              }
+            }
+            if(j+1<map.info.width){
+              if(cells[(j+1)*map.info.width+(i+1)].heuristic<0){
+                cells[(j+1)*map.info.width+(i+1)].heuristic = h+1;//i+1, j+1
+              }
+            }
+          }
+          if(i-1>=0){
+            if(cells[j*map.info.width+(i-1)].heuristic<0){
+              cells[j*map.info.width+(i-1)].heuristic = h+1;//i-1, j
+            }
+            if(j-1>=0){
+              if(cells[(j-1)*map.info.width+(i-1)].heuristic<0){
+                cells[(j-1)*map.info.width+(i-1)].heuristic = h+1;//i-1, j-1
+              }
+            }
+            if(j+1<map.info.width){
+              if(cells[(j+1)*map.info.width+(i-1)].heuristic<0){
+                cells[(j+1)*map.info.width+(i-1)].heuristic = h+1;//i-1, j+1
+              }
+            }
+          }
+        }
+      }
+    }
+    if(!flag){
+      return;
+    }
+    h++;
+  }
 }
