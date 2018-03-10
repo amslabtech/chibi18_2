f0c8b40f5c82cc8224ba42b3d6b0a34bef46edc8
kazuki takahahsi <tkhsh.schoen@gmail.com>
2018-03-10 14:48:45 +0900

    A*実装した。ヒューリスティックの計算に5分かかる。壁から離れない

--- src/global_planner.cpp
+++ src/global_planner.cpp
@@ -20,13 +20,18 @@ public:
     cost = 1;
     heuristic = -1;
     step = 0;
+    sum = -1;
+    parent_index = -1;
+    is_available = -1;
   }
 
   int cost;
   int step;
   int heuristic;
+  int sum;
   int parent_index;
   bool is_wall;
+  bool is_available;
 };
 
 std::vector<Cell> cells;
@@ -41,9 +46,12 @@ void calculate_heuristic(int);
 void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
 {
   map = *msg;
+  std::cout << map.data.size() << std::endl;
   cells.resize(map.info.height*map.info.width);
   for(int i=0;i<map.info.height*map.info.width;i++){
     cells[i].is_wall = (map.data[i]!=0);
+    cells[i].is_available = ((int)map.data[i] > -1);
+    //std::cout << "map.data=" << cells[i].is_available << std::endl;
     if(cells[i].is_wall){
       cells[i].cost = 100;
     }
@@ -51,6 +59,7 @@ void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
   _cost_map.header = map.header;
   _cost_map.info = map.info;
   _cost_map.data.resize(map.info.height*map.info.width);
+  /*
   for(int i=0;i<map.info.height;i++){
     for(int j=0;j<map.info.width;j++){
       std::cout << cells[j*map.info.width+i].is_wall << " ";
@@ -58,34 +67,162 @@ void map_callback(const nav_msgs::OccupancyGridConstPtr& msg)
     std::cout << std::endl;
   }
   std::cout << std::endl;
+  */
   //calculate AStar
   int start_index = get_index(start.point.x, start.point.y);
   int goal_index = get_index(goal.point.x, goal.point.y);
+  std::cout << "calculating heuristic" << std::endl;
   calculate_heuristic(goal_index);
-  open_list.push_back(start_index);
-  while(!open_list.empty()){
-    int n_index = open_list[0];
-    int n = cells[n_index].step + cells[n_index].heuristic;
-    for(int i=0;i<open_list.size();i++){//openlist中で最小の要素を選択
-      if((cells[open_list[i]].step + cells[open_list[i]].heuristic) < n){
-        n_index = i;
-        n = cells[n_index].step + cells[n_index].heuristic;
-      }
-    }
-    if(n_index != goal_index){
-      close_list.push_back(n_index);//選んだものがゴールでなければcloselistへ
-    }else{
-      break;
-    }
-  }
-
+  /*
   for(int i=0;i<map.info.height;i++){
     for(int j=0;j<map.info.width;j++){
       std::cout << std::setw(2) << cells[j*map.info.width+i].heuristic << " ";
     }
     std::cout << std::endl;
   }
+  std::cout << std::endl;
+  */
+  std::cout << "heuristic calculated!" << std::endl;
+  std::cout << "calculating path" << std::endl;
   open_list.push_back(start_index);
+  cells[open_list[0]].sum = cells[open_list[0]].step + cells[open_list[0]].heuristic;
+  while(!open_list.empty()){
+    int n_index = open_list[0];
+    int n = cells[n_index].step + cells[n_index].heuristic;
+    for(int i=0;i<open_list.size();i++){//openlist中で最小の要素を選択
+      if(cells[open_list[i]].sum < n){
+        n_index = open_list[i];
+        n = cells[n_index].sum;
+      }
+    }
+    if(n_index != goal_index){
+      close_list.push_back(n_index);//選んだものがゴールでなければcloselistへ
+      open_list.erase(std::remove(open_list.begin(), open_list.end(), n_index), open_list.end());//openlistから削除
+    }else{
+      break;
+    }
+    int _i = n_index % map.info.width;
+    int _j = (n_index - _i) / map.info.width;
+    int _index;
+    if(_j-1>=0){
+      _index = (_j-1)*map.info.width+_i;//i, j-1
+      if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+        if(!cells[_index].is_wall){
+          cells[_index].step = cells[n_index].step + 1;
+	  cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+          cells[_index].parent_index = n_index;
+          open_list.push_back(_index);
+        }
+      }
+    }
+    if(_j+1<map.info.width){
+      _index = (_j+1)*map.info.width+_i;//i, j+1
+      if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+        if(!cells[_index].is_wall){
+          cells[_index].step = cells[n_index].step + 1;
+          cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+          cells[_index].parent_index = n_index;
+          open_list.push_back(_index);
+        }
+      }
+    }
+    if(_i+1<map.info.height){
+      _index = _j*map.info.width+(_i+1);//i+1, j
+      if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+        if(!cells[_index].is_wall){
+          cells[_index].step = cells[n_index].step + 1;
+          cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+          cells[_index].parent_index = n_index;
+          open_list.push_back(_index);
+        }
+      }
+      if(_j-1>=0){
+        _index = (_j-1)*map.info.width+(_i+1);//i+1, j-1
+        if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+          if(!cells[_index].is_wall){
+            cells[_index].step = cells[n_index].step + 1;
+            cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+            cells[_index].parent_index = n_index;
+            open_list.push_back(_index);
+          }
+        }
+      }
+      if(_j+1<map.info.width){
+        _index = (_j+1)*map.info.width+(_i+1);//i+1, j+1
+        if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+          if(!cells[_index].is_wall){
+            cells[_index].step = cells[n_index].step + 1;
+            cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+            cells[_index].parent_index = n_index;
+            open_list.push_back(_index);
+          }
+        }
+      }
+    }
+    if(_i-1>=0){
+      _index = _j*map.info.width+(_i-1);//i-1, j
+      if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+        if(!cells[_index].is_wall){
+          cells[_index].step = cells[n_index].step + 1;
+          cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+          cells[_index].parent_index = n_index;
+          open_list.push_back(_index);
+        }
+      }
+      if(_j-1>=0){
+        _index = (_j-1)*map.info.width+(_i-1);//i-1, j-1
+        if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+          if(!cells[_index].is_wall){
+            cells[_index].step = cells[n_index].step + 1;
+            cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+            cells[_index].parent_index = n_index;
+            open_list.push_back(_index);
+          }
+        }
+      }
+      if(_j+1<map.info.width){
+        _index = (_j+1)*map.info.width+(_i-1);//i-1, j+1
+        if((std::find(open_list.begin(), open_list.end(), _index) == open_list.end()) && (std::find(close_list.begin(), close_list.end(), _index) == close_list.end())){
+          if(!cells[_index].is_wall){
+            cells[_index].step = cells[n_index].step + 1;
+            cells[_index].sum = cells[_index].cost + cells[_index].step + cells[_index].heuristic;
+            cells[_index].parent_index = n_index;
+            open_list.push_back(_index);
+          }
+        }
+      }
+    }
+  }
+  /*
+  for(int i=0;i<map.info.height;i++){
+    for(int j=0;j<map.info.width;j++){
+      std::cout << std::setw(2) << cells[j*map.info.width+i].sum << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  for(int i=0;i<map.info.height;i++){
+    for(int j=0;j<map.info.width;j++){
+      std::cout << std::setw(2) << cells[j*map.info.width+i].parent_index<< " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  */
+  int path_index = goal_index;
+  geometry_msgs::PoseStamped path_pose;
+  path_pose.pose.orientation.w = 1;
+  path_pose.header.frame_id = "map";
+  while(1){
+    path_pose.pose.position.x = (path_index % map.info.width) * map.info.resolution + map.info.origin.position.x;
+    path_pose.pose.position.y = (path_index - (path_index % map.info.width)) / map.info.width * map.info.resolution + map.info.origin.position.y;
+    global_path.poses.push_back(path_pose);
+    path_index = cells[path_index].parent_index;
+    if(path_index < 0){
+      break;
+    }
+  }
+  std::cout << "global path generated!" << std::endl;
 }
 
 
@@ -106,22 +243,13 @@ int main(int argc, char** argv)
 
   ros::Subscriber map_sub = nh.subscribe("/map", 100, map_callback);
 
-  ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/chibi18/global_path", 100);
+  ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/chibi18/global_path", 100, true);
 
   ros::Publisher cost_pub = nh.advertise<nav_msgs::OccupancyGrid>("/chibi18/cost_map", 100);
 
   ros::Rate loop_rate(10);
 
   global_path.header.frame_id = "map";
-
-
-  geometry_msgs::PoseStamped pose;
-  pose.pose.position.x = 0;
-  pose.pose.position.y = 0;
-  pose.pose.orientation.w = 1;
-  global_path.poses.push_back(pose);
-  pose.pose.position.x = 1;
-  global_path.poses.push_back(pose);
 
   while(ros::ok()){
     if(!map.data.empty()){
@@ -164,52 +292,67 @@ int get_index(float x, float y)
 
 void calculate_heuristic(int goal_index)
 {
+  ros::Time start_time = ros::Time::now();
   int h = 0;
+  int _h = 0;//計算省略用
+  int _index = 0;//計算省略用
   cells[goal_index].heuristic = 0;
   bool flag = false;
   while(1){
     flag = false;
     for(int i=0;i<map.info.height;i++){
       for(int j=0;j<map.info.width;j++){
+        if(!cells[j*map.info.width+i].is_available){
+          continue;
+        }
         if(cells[j*map.info.width+i].heuristic == h){
+          _h = h + 1;
           flag = true;
           if(j-1>=0){
-            if(cells[(j-1)*map.info.width+i].heuristic<0){
-              cells[(j-1)*map.info.width+i].heuristic = h+1;//i, j-1
+            _index = (j-1)*map.info.width+i;
+            if(cells[_index].heuristic<0){
+              cells[_index].heuristic = _h;//i, j-1
             }
           }
           if(j+1<map.info.width){
-            if(cells[(j+1)*map.info.width+i].heuristic<0){
-              cells[(j+1)*map.info.width+i].heuristic = h+1;//i, j+1
+            _index = (j+1)*map.info.width+i;
+            if(cells[_index].heuristic<0){
+              cells[_index].heuristic = _h;//i, j+1
             }
           }
           if(i+1<map.info.height){
-            if(cells[j*map.info.width+(i+1)].heuristic<0){
-              cells[j*map.info.width+(i+1)].heuristic = h+1;//i+1, j
+            _index = j*map.info.width+(i+1);
+            if(cells[_index].heuristic<0){
+              cells[_index].heuristic = _h;//i+1, j
             }
             if(j-1>=0){
-              if(cells[(j-1)*map.info.width+(i+1)].heuristic<0){
-                cells[(j-1)*map.info.width+(i+1)].heuristic = h+1;//i+1, j-1
+              _index = (j-1)*map.info.width+(i+1);
+              if(cells[_index].heuristic<0){
+                cells[_index].heuristic = _h;//i+1, j-1
               }
             }
             if(j+1<map.info.width){
-              if(cells[(j+1)*map.info.width+(i+1)].heuristic<0){
-                cells[(j+1)*map.info.width+(i+1)].heuristic = h+1;//i+1, j+1
+              _index = (j+1)*map.info.width+(i+1);
+              if(cells[_index].heuristic<0){
+                cells[_index].heuristic = _h;//i+1, j+1
               }
             }
           }
           if(i-1>=0){
-            if(cells[j*map.info.width+(i-1)].heuristic<0){
-              cells[j*map.info.width+(i-1)].heuristic = h+1;//i-1, j
+            _index = j*map.info.width+(i-1);
+            if(cells[_index].heuristic<0){
+              cells[_index].heuristic = _h;//i-1, j
             }
             if(j-1>=0){
-              if(cells[(j-1)*map.info.width+(i-1)].heuristic<0){
-                cells[(j-1)*map.info.width+(i-1)].heuristic = h+1;//i-1, j-1
+              _index = (j-1)*map.info.width+(i-1); 
+              if(cells[_index].heuristic<0){
+                cells[_index].heuristic = _h;//i-1, j-1
               }
             }
             if(j+1<map.info.width){
-              if(cells[(j+1)*map.info.width+(i-1)].heuristic<0){
-                cells[(j+1)*map.info.width+(i-1)].heuristic = h+1;//i-1, j+1
+              _index = (j+1)*map.info.width+(i-1);
+              if(cells[_index].heuristic<0){
+                cells[_index].heuristic = _h;//i-1, j+1
               }
             }
           }
@@ -217,8 +360,14 @@ void calculate_heuristic(int goal_index)
       }
     }
     if(!flag){
-      return;
+      break;;
     }
+    if(h > map.info.width*map.info.height){
+      std::cout << "heuristic value error!" << std::endl;
+      exit(-1);
+    }
+    //std::cout << h << std::endl;
     h++;
   }
+  std::cout << ros::Time::now()-start_time << "[s]" << std::endl;
 }
