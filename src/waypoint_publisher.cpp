cd34ad196eecb2d510fa44cc6af3dc0b123af6b5
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-03-23 16:19:21 +0900

    1周分のwaypointを与えるノードを作った

---
+++ src/waypoint_publisher.cpp
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/tf.h>
+
+std::vector<geometry_msgs::PoseStamped> wp;
+
+void set_pose(float, float, float);
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "waypoint_publisher");
+  ros::NodeHandle nh;
+  
+  ros::Rate loop_rate(10);
+
+  ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 100);
+
+  set_pose(11, -1, 0);
+  set_pose(12.5, 13, 0);
+  set_pose(-22, 15, 0);
+  set_pose(-22, 1, 0);
+  set_pose(0, 0, 0);
+
+  for(int i=0;i<wp.size();i++){
+    ros::Duration(2).sleep();
+    pose_pub.publish(wp[i]);
+  }
+
+  while(ros::ok()){
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
+
+void set_pose(float x, float y, float yaw)
+{
+  geometry_msgs::PoseStamped pose;
+  pose.header.frame_id = "map";
+  pose.pose.position.x = x;
+  pose.pose.position.y = y;
+  pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
+  wp.push_back(pose);
+}
